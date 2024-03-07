2149f416a715448d9190c5b925f5d62e62746d26
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-07 21:37:09 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('Header');
-
-}).listen(8080);
+   
+let num1 = 10;
+let num2 = 10;
+let sum = num1 + num2;
+console.log("Sum :", sum);
