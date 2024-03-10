1ff28c116b673a5d45986a948506adbb8da67dc6
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-10 11:33:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://sarvesh:mevo123@testingcluster.tg9uqrx.mongodb.net/?retryWrites=true&w=majority&appName=TestingCluster";
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
+const client = new MongoClient(uri);
 async function run() {
   try {
+    
     const database = client.db("mevo");
-    const users = database.collection("users");
-    const user = {
-        Name: "testUser",
-        Password : "test123"
-    }
-    /*
-    const result = await users.insertOne(user);
-    if(result.acknowledged == true){
-        console.log("Successfully Inserted");
-    }
-    */
+    const movies = database.collection("records");
+
+    const query = { Name: "Akshay Kumar" };
+
+    const options = {
+      projection: { _id: 0, Age: 1, Gender: 1, Name:1 },
+    };
+
+    const movie = await movies.findOne(query,options);
+    
+    console.log(movie);
   } finally {
     await client.close();
   }
