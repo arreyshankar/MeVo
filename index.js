075d19085a38f3fb2de9c3e3ae386950dcc2f17d
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-10 11:55:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
+/*
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 const uri = "mongodb+srv://sarvesh:mevo123@testingcluster.tg9uqrx.mongodb.net/?retryWrites=true&w=majority&appName=TestingCluster";
 const client = new MongoClient(uri);
-async function run() {
-  try {
-    
-    const database = client.db("mevo");
-    const movies = database.collection("records");
 
-    const query = { Name: "Akshay Kumar" };
+*/
 
-    const options = {
-      projection: { _id: 0, Age: 1, Gender: 1, Name:1 },
-    };
 
-    const movie = await movies.findOne(query,options);
-    
-    console.log(movie);
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.post('/data', (req, res) => {
+  const data = req.body.name;
+  // Process the received data
+  res.send('Data received: ' + data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
