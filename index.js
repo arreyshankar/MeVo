8775ba4338982aa00f2c87d71bd0182d9c34c0b5
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-15 20:29:28 +0530

    update

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-/*
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://sarvesh:mevo123@testingcluster.tg9uqrx.mongodb.net/?retryWrites=true&w=majority&appName=TestingCluster";
-const client = new MongoClient(uri);
-
-*/
-
-
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.post('/data', (req, res) => {
   const data = req.body.name;
-  // Process the received data
   res.send('Data received: ' + data);
   console.log('Data is ::' + data)
 });
