bdd10593bb85f934b1808475321bb297cb911d55
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-16 09:41:58 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express"); 
 const app = express(); 
 const PORT = 5000; 
-app.get("/", (req, res) => { 
+app.get("/test", (req, res) => { 
     res.send("Express on Vercel"); 
 }); 
     app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
