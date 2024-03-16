66e35a1250a9d8342f39334661a201c2aeef4ba8
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-16 09:25:32 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-export default async function handler(req, res) {
-    const { body } = req;
-    return res.send(`Hello ${body.name}, you just parsed the request body!`);
-  }
+const express = require('express')
+const app = express()
+const port = 4000
+const testData = {
+  name: 'John Doe',
+  email: 'user@mail.com',
+  password: '12345'
+}
+app.get('/', (req, res) => res.send(testData))
+app.listen(port, () => console.log(`Server listening on port ${port}`))
