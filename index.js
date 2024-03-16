33bbb97bb01afec1526d6db8be509c33bfef2719
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-16 19:53:44 +0530

    update

--- index.js
+++ index.js
@@ -1,14 +1,73 @@
-const express = require("express"); 
-const bodyParser = require('body-parser');
-const app = express(); 
-const PORT = 5000; 
-app.get("/test", (req, res) => { 
-    res.send("Express on Vercel"); 
-}); 
+const express = require('express')
+const app = express()
+const mongoClient = require('mongodb').MongoClient
 
-app.post('/login', (req, res) => {
-    const { param1, param2 } = req.body;
-    console.log(`Data is:: ${param1} and ${param2}`)
-    res.status(200).json({ message: 'Success' });
-  });
-    app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+const url = "mongodb+srv://sarvesh:mevo123@testingcluster.tg9uqrx.mongodb.net/?retryWrites=true&w=majority&appName=TestingCluster"
+app.use(express.json())
+
+mongoClient.connect(url, (err, db) => {
+
+    if (err) {
+        console.log("Error while connecting mongo client")
+    } else {
+
+        const myDb = db.db('mevo')
+        const collection = myDb.collection('users')
+
+        app.post('/signup', (req, res) => {
+
+            const newUser = {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            }
+
+            const query = { email: newUser.email }
+
+            collection.findOne(query, (err, result) => {
+
+                if (result == null) {
+                    collection.insertOne(newUser, (err, result) => {
+                        res.status(200).send()
+                    })
+                } else {
+                    res.status(400).send()
+                }
+
+            })
+
+        })
+
+        app.post('/login', (req, res) => {
+
+            const query = {
+                email: req.body.email, 
+                password: req.body.password
+            }
+
+            collection.findOne(query, (err, result) => {
+
+                if (result != null) {
+
+                    const objToSend = {
+                        name: result.name,
+                        email: result.email
+                    }
+
+                    res.status(200).send(JSON.stringify(objToSend))
+
+                } else {
+                    res.status(404).send()
+                }
+
+            })
+
+        })
+
+    }
+
+})
+
+app.listen(5000, () => {
+    console.log("Listening on port 5000...")
+})
