baa2816367831a73ef43bae75d4c75a167a85683
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-16 09:53:33 +0530

    upfate

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express"); 
+const bodyParser = require('body-parser');
 const app = express(); 
 const PORT = 5000; 
 app.get("/test", (req, res) => { 
     res.send("Express on Vercel"); 
 }); 
+
+app.post('/login', (req, res) => {
+    const { param1, param2 } = req.body;
+    console.log(`Data is:: ${param1} and ${param2}`)
+    res.status(200).json({ message: 'Success' });
+  });
     app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
