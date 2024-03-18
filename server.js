02b5055c80b5fcb9117a84fc9f018aa0fcd1f8ae
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-18 12:16:06 +0530

    update

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-const app = require("express");
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+ 
+app.all('*', (req, res) => {
+    res.send("GET Request Called")
+    console.log("Request++")
+})
+ 
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
